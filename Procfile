78be0dfd9e9d25e9bf70fd95abc8b80d781cbe52
Leonardo <leomleao@gmail.com>
2018-05-27 23:51:10 -0300

    New interceptor for error control

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server/dist/server.js
+web: npm start
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,15 +7,17 @@ import * as path from 'path';
 import { ApplicationModule } from './app.module';
 import { ValidationPipe } from './validation.pipe';
 import { HttpExceptionFilter } from './http-exception.filter';
+import { ErrorsInterceptor } from './interceptors';
 
 async function bootstrap() {
-  if (process.env.NODE_ENV !== 'production') {
-    await dotenv.config({silent: true, path: '../.env'});
+  if (!process.env.NODE_ENV) {
+    await dotenv.config({silent: true, path: path.resolve(__dirname, '../.env')});
   }
   await raven.config('https://ba3ccffd7a0d43048735eb90a7890a2d@sentry.io/1214409').install();
   const app = await NestFactory.create(ApplicationModule, new FastifyAdapter());
   app.use(bodyParser.json());
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ErrorsInterceptor());
   app.useStaticAssets({
     root: path.resolve(__dirname, '../../client/dist'),
   });
