b744cfa98d0f3679c2d4b7f6be827f428f9a6352
Leonardo <leomleao@gmail.com>
2018-05-28 00:06:30 -0300

    No error handler for now

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,8 @@ import * as path from 'path';
 
 import { ApplicationModule } from './app.module';
 import { ValidationPipe } from './validation.pipe';
-import { HttpExceptionFilter } from './http-exception.filter';
-import { ErrorsInterceptor } from './interceptors';
+// import { HttpExceptionFilter } from './http-exception.filter';
+// import { ErrorsInterceptor } from './interceptors';
 
 async function bootstrap() {
   if (!process.env.NODE_ENV) {
@@ -17,13 +17,13 @@ async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule, new FastifyAdapter());
   app.use(bodyParser.json());
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new ErrorsInterceptor());
+  // app.useGlobalInterceptors(new ErrorsInterceptor());
   app.useStaticAssets({
     root: path.resolve(__dirname, '../../client/dist'),
   });
   // app.setGlobalPrefix('api/v1');
-  app.useGlobalFilters(new HttpExceptionFilter());
-  const port = process.env.PORT || 8080;
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 
